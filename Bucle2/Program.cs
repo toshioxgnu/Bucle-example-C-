7ecd91e7b14c7006e53e262f3fbf18ac9ac05f93
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2020-04-05 14:37:43 -0400

    Pequeño ejercicio de bucle para el ramo de programacion II

---
+++ Bucle2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bucle2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = {"Mercedez", "Chevrolet", "Camaro"};
+
+            for (int i = 0 ; i < cars.Length; ++i)
+            {
+                Console.WriteLine(cars[i]);
+            }
+            
+        }
+    }
+}
